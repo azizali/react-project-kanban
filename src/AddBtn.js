43634264540b84b5c9e02337ed35140957c3daf8
Aziz Ali <aziz@ilovecoding.org>
2019-02-15 12:25:39 -0600

    Functional Kanban Board with state

--- src/AddBtn.js
+++ src/AddBtn.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function({ clickHandler }) {
-    return (
-        <button
-            className="btn btn-light mt-2  border-secondary btn-block"
-            onClick={clickHandler}>
-            + Add a Card
-    </button>
-    )
+	return (
+		<button className="btn btn-light mt-2  border-secondary btn-block" onClick={clickHandler}>
+			+ Add a Card
+		</button>
+	);
 }
